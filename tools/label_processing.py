fd7a4995a19c54753df01234be18bbd816ca231c
Edward Rees <edward.r.re.es@zoho.com>
2019-12-03 19:59:50 +0000

    added segmentation

---
+++ tools/label_processing.py
@@ -0,0 +1,159 @@
+'''
+A collection of functions used for processing the labels of the input images
+'''
+
+# Depenancies
+from image_processing import MakeFlat
+from sklearn.cluster import KMeans
+import numpy as np
+from feature_processing import create_pixel_loc
+from tqdm import tqdm
+from skimage.measure import block_reduce
+
+
+def segment_labeled_images(imgs,
+                           n_clusters=6,
+                           label_dict=[],
+                           desired_labels=['eyes', 'mouth', 'nose']):
+    '''
+    Utilility function for segmenting an array of images at once
+    '''
+
+    imgs_out = np.empty(imgs.shape[:3], dtype=np.uint8)
+
+    for i, img in enumerate(tqdm(imgs, desc='Segmenting labels')):
+        seg_img = segment_labeled_image(img, n_clusters, label_dict,
+                                        desired_labels)
+        imgs_out[i] = seg_img
+
+    return imgs_out
+
+
+def segment_labeled_image(img,
+                          n_clusters,
+                          label_dict={},
+                          desired_labels=['eyes', 'mouth', 'nose']):
+
+    # create features
+    make_flat = MakeFlat(img.shape)
+    flat_image = make_flat.transform(img)
+
+    # kmeans
+    kmeans = KMeans(n_clusters=n_clusters)
+    kmeans.fit(flat_image)
+    labels = kmeans.labels_ + 1
+
+    img_labels = make_flat.inverse_transform(labels)
+
+    # Translate the labels into labels
+    img_labels = convert_labels(img_labels, img, label_dict, desired_labels)
+    img_labels = img_labels.astype(np.uint8)
+    img_labels = split_eyes(img_labels, label_dict)
+
+    return img_labels
+
+
+def convert_labels(seg_img, labeled_img, label_dict, desired_labels):
+
+    map_dict = {}
+    for i in label_dict.keys():
+        mask = mask_image(labeled_img,
+                          i,
+                          label_dict,
+                          method='color_label',
+                          return_type='bool')
+
+        masked_seg = seg_img[mask[:, :, 0]]
+        u = np.unique(masked_seg)
+        try:
+            max_ = u[0]
+        except Exception:
+            print('label error')
+
+        if i in desired_labels:
+            map_dict[max_] = [label_dict[i][1], i]
+        else:
+            # if not in desired label make 0
+            map_dict[max_] = [0, i]
+
+    seg_img = np.vectorize(map_label)(seg_img, map_dict)
+    return seg_img
+
+
+def map_label(x, map_dict):
+    y = map_dict[x][0]
+    return y
+
+
+def mask_image(img, criteria, label_dict, method='color', return_type='bool'):
+
+    if method == 'color':
+        make_flat = MakeFlat(img.shape)
+        flat_image = make_flat.transform(img)
+        pixel_values = np.array(
+            [f'{pix[0]}-{pix[1]}-{pix[2]}' for pix in flat_image[:, :3]])
+        mask = pixel_values == criteria
+        mask3 = np.moveaxis(np.array([mask, mask, mask]), 0, 1)
+        if return_type == 'bool':
+            masked_img = make_flat.inverse_transform(mask3)
+        if return_type == 'original':
+            masked_img = make_flat.inverse_transform(
+                np.multiply(flat_image, mask3))
+        if return_type == 'white':
+            masked_img = make_flat.inverse_transform(mask3 * 255)
+
+    elif method == 'color_label':
+        color = label_dict[criteria][0]
+        masked_img = mask_image(img,
+                                color,
+                                label_dict,
+                                method='color',
+                                return_type=return_type)
+
+    elif method == 'label':
+        mask = masked_img == criteria
+        if return_type == 'bool':
+            masked_img = mask
+        if return_type == 'original':
+            masked_img = np.multiple(img, mask)
+        if return_type == 'white':
+            masked_img = img * 255
+
+    else:
+        raise Exception('Method invalid')
+
+    return masked_img
+
+
+def split_eyes(segmented_image, label_dict):
+    eye_label = label_dict['eyes'][1]
+    nose_label = label_dict['nose'][1]
+    eye_mask = segmented_image == eye_label
+    nose_mask = segmented_image == nose_label
+
+    (x, y) = create_pixel_loc(segmented_image.shape)
+    x = x.reshape(segmented_image.shape)
+    y = y.reshape(segmented_image.shape)
+    nose_x = np.median(x[nose_mask].ravel())
+    right_side = x > nose_x
+
+    segmented_image[np.multiply(right_side, eye_mask)] += 1
+
+    return segmented_image
+
+
+def max_pool_labels(imgs, kernel=(2, 2), single_img=False):
+    if single_img:
+        imgs = np.array([imgs])
+
+    out_imgs = np.zeros((imgs.shape[0], imgs.shape[1] // kernel[0],
+                         imgs.shape[2] // kernel[1]),
+                        dtype=np.uint8)
+
+    for i, img in enumerate(imgs):
+        b = block_reduce(img, kernel, np.max)
+        out_imgs[i] = b
+
+    if single_img:
+        return out_imgs[0]
+    return out_imgs
---
+++ tools/ovr_file_sorter.py
@@ -0,0 +1,114 @@
+import os
+from shutil import copyfile
+
+
+def sort_files(source, dest, as_links):
+
+    real_dir = f'{source}/real_and_fake_face/real'
+    fake_dir = f'{source}/real_and_fake_face/fake'
+
+    # Left eye
+    criteria_copy(real_dir, fake_dir, dest, 'left_eye', as_links)
+    # Right eye
+    criteria_copy(real_dir, fake_dir, dest, 'right_eye', as_links)
+    # Mouth
+    criteria_copy(real_dir, fake_dir, dest, 'mouth', as_links)
+    # Nose
+    criteria_copy(real_dir, fake_dir, dest, 'nose', as_links)
+
+
+def copy_files(source,
+               fake_destination=False,
+               real_destination='',
+               criteria=None,
+               as_links=True):
+
+    list_dir = os.listdir(source)
+    list_dir = [i for i in list_dir if '.jpg' in i]
+
+    if not os.path.exists(real_destination):
+        os.makedirs(real_destination)
+
+    if not os.path.exists(fake_destination):
+        os.makedirs(fake_destination)
+
+    for file in list_dir:
+
+        if criteria == 'left_eye':
+            i = 0
+        elif criteria == 'right_eye':
+            i = 1
+        elif criteria == 'nose':
+            i = 2
+        elif criteria == 'mouth':
+            i = 3
+        else:
+            i = 8
+
+        if file[-8 + i] == '1' and fake_destination is not False:
+            destination = os.path.abspath(fake_destination + '/' + file)
+        else:
+            destination = os.path.abspath(real_destination + '/' + file)
+
+        source_file = os.path.abspath(source + '/' + file)
+
+        if as_links is False and not os.path.exists(destination):
+            copyfile(source_file, destination)
+        elif as_links is True and not os.path.exists(destination):
+            os.symlink(source_file, destination)
+
+
+def criteria_copy(real_dir, fake_dir, dest, criteria, as_links):
+
+    if as_links is True:
+        lin = '_links'
+    else:
+        lin = ''
+
+    real_dest = f'{dest}/real_and_fake_face_ovr{lin}/' + criteria + '/real'
+    fake_dest = f'{dest}/real_and_fake_face_ovr{lin}/' + criteria + '/fake'
+
+    # Copy photoshoped files
+    copy_files(fake_dir, fake_dest, real_dest, criteria, as_links)
+
+    # Copy real files
+    copy_files(real_dir, real_destination=real_dest, as_links=as_links)
+
+
+def check_numbers(source, dest, criteria, as_links):
+
+    if as_links is True:
+        lin = '_links'
+    else:
+        lin = ''
+
+    source1 = f'{source}/real_and_fake_face/real'
+    source2 = f'{source}/real_and_fake_face/fake'
+    destination1 = f'{dest}/real_and_fake_face_ovr{lin}/' + criteria + '/real'
+    destination2 = f'{dest}/real_and_fake_face_ovr{lin}/' + criteria + '/fake'
+
+    original = os.listdir(source1) + os.listdir(source2)
+    original = len([i for i in original if '.jpg' in i])
+    copied = os.listdir(destination1) + os.listdir(destination2)
+    copied = len([i for i in copied if '.jpg' in i])
+
+    assert original == copied
+    return copied
+
+
+if __name__ == '__main__':
+
+    # Sort files
+    source = './ml-database'
+    dest = './data'
+    as_links = True
+    sort_files(source, dest, as_links)
+
+    # Check numbers of copied files
+    number1 = check_numbers(source, dest, 'left_eye', as_links)
+    number2 = check_numbers(source, dest, 'right_eye', as_links)
+    number3 = check_numbers(source, dest, 'mouth', as_links)
+    number4 = check_numbers(source, dest, 'nose', as_links)
+
+    assert number1 == number2 == number3 == number4
+    print(f'Copied {number1} images successfully')
